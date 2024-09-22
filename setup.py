77b8f3b3eb574db95563597cbf12745da0dcd757
Anusha Bishop <46795148+AnushaPB@users.noreply.github.com>
2024-09-21 19:33:20 -0700

    Update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="evodoodle",
-    version="0.0.0",
+    version="1.0.0",
     author="Anusha Bishop",
     author_email="anusha.bishop@berkeley.edu",
     description="A doodling game for understanding evolution across space",
